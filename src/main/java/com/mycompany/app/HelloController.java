04a6fcd51f29c157ba97955c5e0ed15b6ee10e1d
zzkskys <2336276765@qq.com>
2021-02-18 18:49:27 +0800

    测试 master 分支自动构建

--- src/main/java/com/mycompany/app/HelloController.java
+++ src/main/java/com/mycompany/app/HelloController.java
@@ -1,9 +1,12 @@
 package com.mycompany.app;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.message.StringFormattedMessage;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/")
@@ -14,7 +17,7 @@ public class HelloController {
 
     @GetMapping
     public String hello() {
-        return "你好 " + helloProperties.getName() + "!!";
+        return "你好 " + helloProperties.getName() + ", 欢迎您的访问 !";
     }
 
 
